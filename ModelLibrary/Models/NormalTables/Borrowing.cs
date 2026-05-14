370ba9f5dbb74f156e2ca3e3b0b27e3761b75eb4
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-05-14 17:04:32 +0530

    borrowing table added

--- ModelLibrary/Models/NormalTables/Borrowing.cs
+++ ModelLibrary/Models/NormalTables/Borrowing.cs
@@ -1,1 +1,15 @@
 namespace LibraryManagement.ModelLibrary.Models;
+
+public class Borrowing
+{
+    public int BorrowingId {get;set;}
+    public int MemberId {get;set;}
+    public Member? Member {get;set;}
+    public int BookCopyId {get;set;}
+    public BookCopy? BookCopy {get;set;}
+
+    public DateTime BorrowedDate {get;set;}
+    public DateTime DueDate {get;set;}
+    public DateTime ReturnDate {get;set;}
+    public required BookStatus BookStatus {get;set;}
+}
