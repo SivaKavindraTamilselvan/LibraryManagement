9947a4448c98aa321f5cae2d6958484ad3f84ee6
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-05-14 23:55:03 +0530

    added the modelbuilder for borrowingstatus

--- DataAccessLibrary/DBContext/DBContext.cs
+++ DataAccessLibrary/DBContext/DBContext.cs
@@ -16,10 +16,10 @@ public class LibraryManagementContext : DbContext
     {
         modelBuilder.Entity<Role>(r =>
         {
-           r.HasKey(r=>r.RoleId).HasName("PK_Role_Id");
-           r.HasIndex(r=>r.RoleName).IsUnique();
-           r.HasData(new Role() {RoleId = 1,RoleName = "Admin"});
-           r.HasData(new Role() {RoleId = 2,RoleName = "User"}) ;
+            r.HasKey(r => r.RoleId).HasName("PK_Role_Id");
+            r.HasIndex(r => r.RoleName).IsUnique();
+            r.HasData(new Role() { RoleId = 1, RoleName = "Admin" });
+            r.HasData(new Role() { RoleId = 2, RoleName = "User" });
         });
 
         modelBuilder.Entity<MemberType>(mt =>
@@ -32,11 +32,21 @@ public class LibraryManagementContext : DbContext
 
         modelBuilder.Entity<BookStatus>(bs =>
         {
-            bs.HasKey(bs=>bs.BookStatusId).HasName("PK_Book_Status");
-            bs.HasData(new BookStatus() {BookStatusId =1,BookStatusName = "Available"});
-            bs.HasData(new BookStatus() {BookStatusId =2,BookStatusName = "Unavailable"});
-            bs.HasData(new BookStatus() {BookStatusId =3,BookStatusName = "Lost"});
-            bs.HasData(new BookStatus() {BookStatusId =4,BookStatusName = "Damaged"});
+            bs.HasKey(bs => bs.BookStatusId).HasName("PK_Book_Status");
+            bs.HasIndex(bs=>bs.BookStatusName).IsUnique();
+            bs.HasData(new BookStatus() { BookStatusId = 1, BookStatusName = "Available" });
+            bs.HasData(new BookStatus() { BookStatusId = 2, BookStatusName = "Unavailable" });
+            bs.HasData(new BookStatus() { BookStatusId = 3, BookStatusName = "Lost" });
+            bs.HasData(new BookStatus() { BookStatusId = 4, BookStatusName = "Damaged" });
+        });
+
+        modelBuilder.Entity<BorrowingStatus>(bs =>
+        {
+            bs.HasKey(bs => bs.BorrowingStatusId).HasName("PK_Borrowing_Status");
+            bs.HasIndex(bs=>bs.BorrowingStatusName).IsUnique();
+            bs.HasData(new BorrowingStatus() { BorrowingStatusId = 1, BorrowingStatusName = "Borrowed" });
+            bs.HasData(new BorrowingStatus() { BorrowingStatusId = 2, BorrowingStatusName = "Returned" });
+            bs.HasData(new BorrowingStatus() { BorrowingStatusId = 3, BorrowingStatusName = "OverDue" });
         });
     }
 }
