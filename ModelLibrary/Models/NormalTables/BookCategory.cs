28955eda3e0fd920dc869dd6a1a2b7f7a778713f
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-05-14 16:39:44 +0530

    add the booking category

---
+++ ModelLibrary/Models/NormalTables/BookCategory.cs
@@ -0,0 +1,7 @@
+namespace LibraryManagement.ModelLibrary.Models;
+
+public class BookCategory
+{
+    public int BookingCategoryId {get;set;}
+    public string BookCategoryName {get;set;} = string.Empty;
+}
