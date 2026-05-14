b19f58967e88fad65599998eb665208a2038d732
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-05-15 01:00:01 +0530

    added every migrations in the database

--- ModelLibrary/obj/Debug/net10.0/ModelLibrary.AssemblyInfo.cs
+++ ModelLibrary/obj/Debug/net10.0/ModelLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModelLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+370ba9f5dbb74f156e2ca3e3b0b27e3761b75eb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0b733fc65193c836d77dbd187c75d4cb8681ecd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModelLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModelLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
