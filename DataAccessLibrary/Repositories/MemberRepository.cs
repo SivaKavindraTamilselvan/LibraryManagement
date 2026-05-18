27693c22e57f177ad16fa844455f5833dc5824e7
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-05-18 16:36:53 +0530

    changed the library context refresh and screenshots

--- DataAccessLibrary/Repositories/MemberRepository.cs
+++ DataAccessLibrary/Repositories/MemberRepository.cs
@@ -10,41 +10,42 @@ public class MemberRepository : AbstractRepository<int, Member>
 {
     public override Member? Get(int MemberId)
     {
-        var member = libraryManagementContext.Member.Include(r=>r.Role).Include(mt=>mt.MemberType).Where(m=>m.MemberId == MemberId).FirstOrDefault();
+        var member = libraryManagementContext.Member.Include(r => r.Role).Include(mt => mt.MemberType).Where(m => m.MemberId == MemberId).FirstOrDefault();
         return member;
     }
 
     public List<Member> GetAllMembers()
     {
-        var member = libraryManagementContext.Member.Include(r=>r.Role).Include(mt=>mt.MemberType).ToList();
+        var member = libraryManagementContext.Member.Include(r => r.Role).Include(mt => mt.MemberType).ToList();
         return member;
     }
     public Member? GetMemberByEmail(string email)
     {
-        var member = libraryManagementContext.Member.Include(r=>r.Role).Include(mt=>mt.MemberType).Where(m=>m.Email == email).FirstOrDefault();
+        var member = libraryManagementContext.Member.Include(r => r.Role).Include(mt => mt.MemberType).Where(m => m.Email == email).FirstOrDefault();
         return member;
     }
     public Member? GetMemberByPhoneNumber(string PhoneNumber)
     {
-        var member = libraryManagementContext.Member.Include(r=>r.Role).Include(mt=>mt.MemberType).Where(m=>m.PhoneNumber == PhoneNumber).FirstOrDefault();
+        var member = libraryManagementContext.Member.Include(r => r.Role).Include(mt => mt.MemberType).Where(m => m.PhoneNumber == PhoneNumber).FirstOrDefault();
         return member;
     }
 
     public List<Member> GetMemberByRole(int RoleId)
     {
-        var member = libraryManagementContext.Member.Include(r=>r.Role).Include(mt=>mt.MemberType).Where(m=>m.RoleId == RoleId).ToList();
+        var member = libraryManagementContext.Member.Include(r => r.Role).Include(mt => mt.MemberType).Where(m => m.RoleId == RoleId).ToList();
         return member;
     }
 
     public Member? DeactivateMember(int memberId)
     {
         using var context = new LibraryManagementContext();
-        using var transaction = context.Database.BeginTransaction();
+
         try
         {
-            context.Database.ExecuteSqlInterpolated($"CALL deactivate_member({memberId})");
-            transaction.Commit();
-            var member = context.Member.AsNoTracking().Where(b => b.MemberId == memberId).FirstOrDefault();
+            libraryManagementContext.Database.ExecuteSqlInterpolated($"CALL deactivate_member({memberId})");
+            libraryManagementContext.ChangeTracker.Clear();
+
+            var member = libraryManagementContext.Member.AsNoTracking().Where(b => b.MemberId == memberId).FirstOrDefault();
             return member;
         }
         catch (PostgresException ex)
@@ -53,7 +54,7 @@ public class MemberRepository : AbstractRepository<int, Member>
         }
         catch (Exception ex)
         {
-            transaction.Rollback();
+
             Console.WriteLine(ex.Message);
         }
         return null;
--- ModelLibrary/obj/Debug/net10.0/ModelLibrary.AssemblyInfo.cs
+++ ModelLibrary/obj/Debug/net10.0/ModelLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModelLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5f5ea301389fe9d9208339f8434a72a4015f602")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbd261f6c3944f29addbc190f9b0e0525807bf6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModelLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModelLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
